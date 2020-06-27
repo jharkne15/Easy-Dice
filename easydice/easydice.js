55851d5482f0afc2b42ba276b0f6ed6c9d56a98a
jharkne15 <66063918+jharkne15@users.noreply.github.com>
2020-06-27 15:49:26 -0400

    Skeleton Upload

---
+++ easydice/easydice.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === '!ching') {
+		message.channel.send('chong');
+	}
+	if (message.content === '!mayo') {
+		message.channel.send('naise');
+	}
+	if (message.content === '!vote') {
+		message.channel.send('Vote Joe Budden for President 2020');
+	}
+});
+
+
+client.login('NzI1OTAxODM4NDYwNjQ5NTE0.XvZxUw.tICujXtP-dHxJ0YI-wlIahZvAqI');
